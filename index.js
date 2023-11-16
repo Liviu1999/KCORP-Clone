8ef1cf6f13ff26d137d775979f290695a260ff88
Liviu1999 <101752562+Liviu1999@users.noreply.github.com>
2023-11-16 15:01:40 +0100

    new components

--- index.js
+++ index.js
@@ -12,3 +12,34 @@ menu.addEventListener("click", () => {
     t = true;
   }
 });
+
+const prevBut = document.querySelector("#prev-but-hor");
+const nextBut = document.querySelector("#next-but-hor");
+const tabImages = [];
+tabImages[0] = "../images/produits/maillot/DSC06822.webp";
+tabImages[1] = "../images/produits/maillot/DSC06836-2 (1).webp";
+tabImages[2] = "../images/produits/maillot/DSC06850.webp";
+tabImages[3] = "../images/produits/maillot/DSC06873.webp";
+tabImages[4] = "../images/produits/maillot/DSC06885.webp";
+tabImages[5] = "../images/produits/maillot/DSC06899.webp";
+tabImages[6] = "../images/produits/maillot/KC_FLAT_04405.webp";
+let firstImg = document.querySelector("#first-image");
+
+let index = 6;
+prevBut.addEventListener("click", () => {
+  console.log("hey");
+  if (index == 0) {
+    index = 6;
+  }
+  index--;
+  firstImg.src = `${tabImages[index]}`;
+});
+
+nextBut.addEventListener("click", () => {
+  console.log("hey1");
+  if (index == 6) {
+    index = 0;
+  }
+  index++;
+  firstImg.src = `${tabImages[index]}`;
+});
